49e4198c2d15d3fc875d9cf9d156e9b6955db041
Brian Smith <bwsmith.lingust@gmail.com>
2020-06-19 09:49:00 -0700

    Deleted outdated file myapp.app

--- website/my_app.py
+++
@@ -1,9 +0,0 @@
-from classifier import classify_stuff
-from recommender import recommend_stuff
-
-image = load_image()
-
-image_label = classify_stuff(image)
-recommendation = recommend_stuff(image_label)
-
-print(image_label, recommendation)
