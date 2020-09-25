deabbcee662bb9b9b156c18bbda061dc968a0350
Brian Smith <bwsmith.lingust@gmail.com>
2020-09-25 00:28:52 -0700

    update to match streamlit version

--- app/application.py
+++ app/application.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import InputRequired
-from recommender_final import recommend_game, get_games
+from recommender_final import *
 import pandas as pd
 import re
 
@@ -11,6 +11,71 @@ app.config['SECRET_KEY'] = 'our very hard to guess secret key'
 
 class LoginForm(FlaskForm):
     steam_id_form = StringField('', validators=[InputRequired()], default='76561198026189780')
+
+def get_recs_table(steam_id, my_key):
+    '''
+    Takes a steam id and steam key and calculates recommendations
+    Returns a dataframe with all of the recommendation information:
+    playing time, game, complexity, bgg ranking
+    '''
+    user_games = get_games(steam_id, my_key)
+    if type(user_games) is not str:
+        user_games = normalize_ratings(user_games)
+        sim_matrix = pd.read_csv('sim_matrix.txt', index_col=0)
+        bg_averages_df = pd.read_csv('bg_average_ratings.csv')
+        game_info = pd.read_csv('game_info_lookup.csv', dtype='str', index_col=0)
+
+        known_games = list(sim_matrix.columns)
+
+        predicted_ratings = predict_bg_ratings(known_games, user_games,
+                                               sim_matrix, bg_averages_df)
+        table_to_print = pd.DataFrame(zip(known_games, predicted_ratings),
+                                      columns=['game', 'rating'])
+        table_to_print.sort_values(by=['rating'], ascending=False, inplace=True)
+        table_to_print.reset_index(inplace=True)
+
+        game_info = game_info.drop_duplicates(subset='Name', keep='last')
+        table_to_print = table_to_print.merge(game_info, how='left',
+                                              left_on='game', right_on='Name')
+
+        table_to_print['Your ranking'] = pd.Series(table_to_print.index).apply(lambda x: '#'+str(x + 1))
+        table_to_print['Ranking'] = table_to_print.Ranking.str.replace('[.]0', '')
+        table_to_print['Ranking'] = table_to_print.Ranking.apply(lambda x: '#'+str(x))
+        table_to_print['Ranking'] = table_to_print.Ranking.str.replace('#nan', '?')
+        table_to_print['Complexity'] = table_to_print.Ranking.str.replace('#nan', '?')
+
+        table_to_print = table_to_print[['Game', 'Your ranking',  'Ranking', 'Players', 'Time', 'Complexity']]
+
+        table_to_print.rename(columns={'Your ranking': 'Your ranking',
+                          'Time': 'Playing time',
+                          'Complexity': 'Complexity<br>(out of 5)',
+                          'Game': 'Game',
+                          'Ranking': 'BGG ranking'}, inplace=True)
+
+        return table_to_print
+    elif user_games == 'No games':
+        return('Error1')
+    else:
+        return('Error2')
+
+def render_table(table, n_games=20):
+    """
+    Takes a dataframe of recommendation information
+    Returns a table formatted in HTML
+    """
+    table_head = table.head(n_games)
+    table_to_print = table_head.to_html(index=False, escape=False,
+                                        justify='center', classes='sortable')
+    table_to_print = re.sub('<th>(?=Your ranking</th>)',
+                            '<th class="sorttable_numeric">', table_to_print)
+    table_to_print = re.sub('<th>(?=BGG ranking</th>)',
+                            '<th class="sorttable_numeric">', table_to_print)
+    table_to_print = re.sub('<th>(?=Playing time</th>)',
+                            '<th class="sorttable_numeric">', table_to_print)
+    table_to_print = re.sub('<td>(?=\?</td>)',
+                            '<td sorttable_customkey="99999999">',
+                            table_to_print)
+    return table_to_print
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
@@ -22,31 +87,16 @@ def form():
 @app.route('/<steam_id>', methods=['GET', 'POST'])
 def index(steam_id):
     my_key = open('key.txt').read()
-    user_games = get_games(steam_id, my_key)
-    if type(user_games) is not str:
-        known_games = pd.read_csv('bgg_steam_data_normed.csv')
-        x = recommend_game(user_games, known_games)
-        game_info = pd.read_csv('game_info_lookup.csv', dtype='str', index_col=0)
-        x = x.merge(game_info, how='left', left_on='game', right_on='Name')
-        x['Your ranking'] = pd.Series(x.index).apply(lambda x: '#'+str(x + 1))
-        x = x[['Game', 'Your ranking',  'Ranking', 'Players', 'Time', 'Complexity']]
-        x['Ranking'] = x.Ranking.str.replace('[.]0', '')
-        x['Ranking'] = x.Ranking.apply(lambda x: '#'+str(x))
-        x['Ranking'] = x.Ranking.str.replace('#nan', '?')
-        x.rename(columns={'Time': 'Playing time',
-                          'Complexity': 'Complexity<br>(out of 5)',
-                          'Game': 'Game',
-                          'Ranking': 'BGG ranking'}, inplace=True)
-        table_to_print = x.head(20).to_html(index=False, escape=False, justify='center', classes='sortable')
-        table_to_print = re.sub('<th>(?=Your ranking</th>)', '<th class="sorttable_numeric">', table_to_print)
-        table_to_print = re.sub('<th>(?=BGG ranking</th>)', '<th class="sorttable_numeric">', table_to_print)
-        table_to_print = re.sub('<th>(?=Playing time</th>)', '<th class="sorttable_numeric">', table_to_print)
-        table_to_print = re.sub('<td>(?=\?</td>)', '<td sorttable_customkey="99999999">', table_to_print)
-        return render_template('form.html',  output=table_to_print)
-    elif user_games == 'No games':
-        return('Steam profile set to hidden.')
+    recs_table = get_recs_table(steam_id, my_key)
+
+    if type(recs_table) is str:
+        if recs_table == 'Error1':
+            return('This user doesn\'t have any accessible game information. Maybe their profile is set to hidden.')
+        elif recs_table == 'Error2':
+            return('This isn\'t a valid Steam ID.')
     else:
-        return('This doesn\'t seem to be a valid Steam id')
+        table_to_print = render_table(recs_table, 20)
+        return render_template('form.html', output=table_to_print)
 
 if __name__ == '__main__':
     app.run(debug=False)
--- app/recommender_final.py
+++ app/recommender_final.py
@@ -1,28 +1,13 @@
 import pandas as pd
 import numpy as np
-from surprise import Reader, Dataset, KNNBasic
 import requests as rq
 import json
 
-def recommend_game(user_games, known_games):
-    new_user = user_games
-    steam_id = set(new_user.user).pop()
-    new_user = new_user[new_user.game.isin(known_games.game)]
-    all_data = known_games.append(new_user, ignore_index=True)
-    reader = Reader(rating_scale=(1, 10))
-    data = Dataset.load_from_df(all_data[['user', 'game', 'rating']], reader)
-    trainset = data.build_full_trainset()
-    algo = KNNBasic(sim_options={'name':'cosine', 'user_based':'False'})
-    algo.fit(trainset)
-    list_of_known_board_games = known_games[known_games.source=='bgg'].game.unique()
-    testset = [(steam_id, board_game, '?') for board_game in list_of_known_board_games]
-    predictions = algo.test(testset)
-    board_game_predictions = [(prediction.iid, prediction.est) for prediction in predictions]
-    best_board_games = pd.DataFrame(board_game_predictions, columns=['game','rating'])
-    best_board_games.sort_values(by='rating', ascending=False, inplace=True)
-    return(best_board_games)
-
 def get_games(steam_id, my_key):
+    '''
+    Takes a steam id and api key and returns a dataframe of video game play times
+    for the user
+    '''
     req = rq.get('https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?key={key}&steamid={id}&include_played_free_games=1&include_appinfo=1'.format(key=my_key, id=steam_id))
     if req.status_code == 500:
         return("500")
@@ -33,17 +18,78 @@ def get_games(steam_id, my_key):
         for game in games_list:
             game.update({'user': steam_id})
         user_games = pd.DataFrame(games_list)
-        user_games = user_games[user_games.playtime_forever>10]
-        user_games['game'] = ['steam'+str(game) for game in user_games.appid]
-        user_games['rating'] = np.log(user_games.playtime_forever)
+        user_games['game'] = ['steam' + str(game) for game in user_games.appid]
+        user_games['rating'] = user_games.playtime_forever
         user_games = user_games[['game', 'rating', 'user']]
         return(user_games)
 
+def normalize_ratings(user_games):
+    '''
+    Takes a user-game-rating dataframe and adds a column named
+    'rating_normed' with normalized ratings
+    '''
+
+    # Must play for at least 10 minutes
+    user_games = user_games[user_games.rating>10]
+
+    # Log transform (playtimes are very left skewed)
+    user_games['rating'] = np.log(user_games.rating)
+
+    # Normalize so mean = 5.5 and sd = 1.5
+    ratings = user_games.rating.copy()
+    mean = user_games.rating.mean()
+    sd = user_games.rating.std()
+    user_games['rating_normed'] =  (ratings - mean) / (.75 * sd + 0.0000000000001)
+    user_games['rating_normed'] = user_games['rating_normed'] + 5.5
+
+    # Make sure every rating is between 0 and 1
+    user_games.loc[user_games.rating_normed < 1, 'rating_normed'] = 1
+    user_games.loc[user_games.rating_normed > 10, 'rating_normed'] = 10
+    return(user_games)
 
 
+def predict_bg_ratings(boardgame_names, user_game_ratings, similarity_matrix,
+                       bg_averages_df, min_support=5, k_neighbors=25):
+    '''
+    Takes a list of boardgames and returns a list of predicted values
 
+    Parameters:
+        boardgame_names = list of boardgames
+        user_game_ratings = dataframe with three columns (user, game, rating_normed)
+        similarity_matrix = a matrix with rows as vgs and columns as bgs
+        min_support = minimum number of neighbors for rating prediction
+        k_neighbors = max number of neighbors for rating prediction
+    '''
 
+    # Games must be in dataset
+    user_game_ratings = user_game_ratings[user_game_ratings.game.isin(similarity_matrix.index)]
 
+    # Only consider video games the user has rated
+    similarity_matrix = similarity_matrix.loc[user_game_ratings.game]
+    boardgame_ratings = []
+    for boardgame in boardgame_names:
+        try:
+            all_similar_games = similarity_matrix.loc[
+                similarity_matrix[boardgame] > 0, boardgame]
+        except:
+            predicted_rating = np.nan
+            boardgame_ratings.append(predicted_rating)
+            continue
+        k_similar_games = all_similar_games.sort_values(ascending=False)[
+                          0:k_neighbors]
+        if len(k_similar_games) < min_support:
+            predicted_rating = bg_averages_df.loc[
+                bg_averages_df.game == boardgame, 'rating_normed'].values[0]
+        else:
+            k_similar_games_ratings = user_game_ratings.loc[
+                user_game_ratings.game.isin(
+                    k_similar_games.index), 'rating_normed']
+            weighted_sum = np.dot(k_similar_games_ratings, k_similar_games)
+            total = k_similar_games.sum()
+            weighted_average = weighted_sum / total
+            predicted_rating = weighted_average
+        boardgame_ratings.append(predicted_rating)
+    return boardgame_ratings
 
 
 
